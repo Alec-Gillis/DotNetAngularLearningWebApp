d5eb9af815262b3eb84377754daee2020cb896bf
Alec Gillis <gillisa3@msu.edu>
2021-03-09 23:18:47 -0500

    Add project files.

---
+++ DotNetLearningWebApp/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetLearningWebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Message()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int secretNumber=13)
+        {
+            ViewBag.Name = name;
+            ViewBag.SecretNumber = secretNumber;
+            return View();
+        }
+    }
+}
